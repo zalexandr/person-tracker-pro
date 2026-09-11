af6f2b94a90c33cabf16790ecb7149bdad1d8249
zalexandr <zadera.alex@gmail.com>
2026-09-11 04:57:33 +0200

    Fix binary sensor Ruff errors

--- custom_components/person_tracker_pro/binary_sensor.py
+++ custom_components/person_tracker_pro/binary_sensor.py
@@ -2,9 +2,10 @@
 
 from __future__ import annotations
 
-from typing import Any
-
-from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
