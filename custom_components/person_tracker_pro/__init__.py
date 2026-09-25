304ae8df11c414d9f23cb5eeb88d620adafcd856
zalexandr <zadera.alex@gmail.com>
2026-09-25 11:26:31 +0200

    feat(card): release native map in 0.5.0

--- custom_components/person_tracker_pro/__init__.py
+++ custom_components/person_tracker_pro/__init__.py
@@ -33,7 +33,7 @@ PRIVACY_SCHEMA = vol.Schema(
         vol.Optional("entry_id"): cv.string,
     }
 )
-CARD_VERSION = "0.4.9"
+CARD_VERSION = "0.5.0"
 CARD_URL = f"/api/person_tracker_pro/person-tracker-pro-card-v2.js?v={CARD_VERSION}"
 CARD_V2_URL = "/api/person_tracker_pro/person-tracker-pro-card-v2.js"
 CARD_LOADER_URL = "/api/person_tracker_pro/person-tracker-pro-card-loader.js"
